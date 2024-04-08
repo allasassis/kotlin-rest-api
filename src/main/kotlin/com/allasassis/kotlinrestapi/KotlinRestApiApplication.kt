39e6ccb3d15a4d04b6a02552945b81659111523a
Allas Assis <allasassis10@gmail.com>
2024-04-08 20:37:05 -0300

    main: Project started

---
+++ src/main/kotlin/com/allasassis/kotlinrestapi/KotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.allasassis.kotlinrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/allasassis/kotlinrestapi/KotlinRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.allasassis.kotlinrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
